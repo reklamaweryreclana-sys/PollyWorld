ba0cdb8123357ca8ced1afbf6dbc0eda16a192f1
reklamaweryreclana-sys <reklamaweryreclana@gmail.com>
2026-07-27 16:53:54 +0300

    Delete bootstrap.ts

--- bootstrap.ts
+++
@@ -1,8 +0,0 @@
-import { COOKIE_NAME } from "@/lib/auth";
-import { redirectTo } from "@/lib/redirect";
-
-export async function POST() {
-  const response = redirectTo("/");
-  response.cookies.delete(COOKIE_NAME);
-  return response;
-}
